2b22ee35b3ff6bba93cb6d1c99d7c2e122bf652a
Nikolay <nikolay.kovzik@gmail.com>
2022-03-08 16:05:52 +0300

    form added

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+//const API_KEY = "ba982e3f1b2533d1f07c769f25d59182";
+//const BASE_URL = "http://data.fixer.io/api/";
+const API_KEY = "3f8166cfef574c9eb1486f502eb97162";
+const BASE_URL = "https://openexchangerates.org/api/";
+
+
+
